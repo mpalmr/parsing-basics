a03424fb4410bf52fe8129b0a7128c917bdf9549
mpalmr <matthew.palmer1@mohawkcollege.ca>
2024-07-16 11:01:59 -0400

    add span and tests for lexer's tokens

---
+++ src/lexer/token/mod.rs
@@ -0,0 +1,82 @@
+mod kind;
+mod span;
+
+use self::{span::Span, kind::TokenKind};
+use std::fmt;
+
+#[derive(Eq, PartialEq, Copy, Clone, Hash)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
+
+impl Token {
+    pub fn len(&self) -> usize {
+        (self.span.end - self.span.start) as usize
+    }
+
+    pub fn text<'input>(&self, input: &'input str) -> &'input str {
+        &input[self.span]
+    }
+}
+
+impl fmt::Debug for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} - <{}, {}>", self.kind, self.span.start, self.span.end)
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.kind)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::T;
+    use super::*;
+
+    #[test]
+    fn token_len() {
+        let token = Token {
+            kind: T![==],
+            span: Span {
+                start: 0,
+                end: 2,
+            },
+        };
+        assert_eq!(token.len(), 2);
+
+        let token = Token {
+            kind: T![ident],
+            span: Span {
+                start: 2,
+                end: 7,
+            },
+        };
+        assert_eq!(token.len(), 5);
+    }
+
+    #[test]
+    fn token_text() {
+        let input = "let x = 42;";
+        let token = Token {
+            kind: T![ident],
+            span: Span {
+                start: 4,
+                end: 5,
+            },
+        };
+        assert_eq!(token.text(input), "x");
+
+        let token = Token {
+            kind: T![=],
+            span: Span {
+                start: 6,
+                end: 7,
+            },
+        };
+        assert_eq!(token.text(input), "=");
+    }
+}
---
+++ src/lexer/token/span.rs
@@ -0,0 +1,32 @@
+use std::ops::{Index, Range};
+
+#[derive(Eq, PartialEq, Clone, Copy, Hash, Default, Debug)]
+pub struct Span {
+    // inclusive
+    pub start: u32,
+    // exclusive
+    pub end: u32,
+}
+
+impl From<Span> for Range<usize> {
+    fn from(span: Span) -> Self {
+        span.start as usize..span.end as usize
+    }
+}
+
+impl From<Range<usize>> for Span {
+    fn from(range: Range<usize>) -> Self {
+        Self {
+            start: range.start as u32,
+            end: range.end as u32,
+        }
+    }
+}
+
+impl Index<Span> for str {
+    type Output = str;
+
+    fn index(&self, index: Span) -> &Self::Output {
+        &self[Range::<usize>::from(index)]
+    }
+}
